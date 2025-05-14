904ab435481289edd1e18e9745cb1efa6be9a6c9
Mickael <mickaelgomesconsulting@gmail.com>
2025-05-14 12:30:59 +0200

    New jokes created

--- jokes/clumsy.ts
+++ jokes/clumsy.ts
@@ -10,4 +10,5 @@ export default [
   "Yo mama's so clumsy, her shadow avoids her.",
 
   "Yo mama's so clumsy, she tried to gracefully exit a room and accidentally invented a new Olympic sport: controlled face-planting",
-  "Yo mama's so clumsy, she knocked over the Jenga tower before it was even built",];
+  "Yo mama's so clumsy, she knocked over the Jenga tower before it was even built",
+  "Yo mama is so clumsy, she's been trying to breakdance since 1985 and finally perfected the art of flailing while standing still",];
